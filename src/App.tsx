abe4ea7e6b572130568dd261167757a8989bd2ae
折田幸夫 <3204704@gmail.com>
2021-01-09 20:15:35 +0900

    remove unused comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,9 @@ import "./App.css";
 import HogeComponent from "./HogeComponent";
 import { HogeData } from "./hogeData";
 
-// TODO eslint / prettier を入れないとerrorが出ないっぽい？
-// cosmeと同じルールで縛る必要がある。
 const id = "";
 
 function App() {
-  // const sampleData = HogeData[id];
-  // console.log("sampleDataは？ : ", sampleData.description);
   const descriptionData = HogeData[id];
   return (
     <div className="App">
