62cc583a0cf95630a2c547946604e4becf6693ed
折田幸夫 <3204704@gmail.com>
2021-01-09 17:57:55 +0900

    add mock data

---
+++ src/hogeData.ts
@@ -0,0 +1,13 @@
+
+export const HogeData: {
+    [key: string]: {
+      description: string;
+    };
+  } = {
+    first: {
+      description: "ほげほげほげ"
+    },
+    second:{
+        description: "ふがふがふが"
+    }
+  };
